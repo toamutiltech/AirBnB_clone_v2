6f50fe301db29dbc037e59668bcecdba666529c5
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2023-07-09 15:39:38 -0700

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,15 +3,12 @@
 Distributes an archive to my web servers,
 using the function do_deploy
 """
-from fabric.api import env, run, put, task
-from fabric.context_managers import settings
-from fabric.contrib.files import exists
-import argparse
+from fabric.api import *
 from datetime import datetime
 import os
 
 env.hosts = ['100.26.247.104', '34.224.16.213']
-env.warn_only = True
+env.user = 'ubuntu'
 
 
 def do_pack():
@@ -31,67 +28,26 @@ def do_pack():
         return None
 
 
-
-def do_deploy(archive_path, ssh_key, username):
-    """
-    Distributes an archive to the web servers
-    """
-
-    env.key_filename = ssh_key
-    env.user = username
-
-    if not exists(archive_path):
+def do_deploy(archive_path):
+    '''
+    Deploy archive to web server
+    '''
+    if not os.path.exists(archive_path):
         return False
-
+    file_name = archive_path.split('/')[1]
+    file_path = '/data/web_static/releases/'
+    releases_path = file_path + file_name[:-4]
     try:
-        # Upload the archive to the temporary folder on the server
-        put(archive_path, "/tmp/")
-
-        # Extract the filename from the archive path
-        archive_filename = archive_path.split('/')[-1]
-        archive_folder = archive_filename.split('.')[0]
-
-        # Create the destination directory for the archive files
-        run("mkdir -p /data/web_static/releases/{}/".format(archive_folder))
-
-        # Uncompress the archive into the destination directory
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(archive_filename, archive_folder))
-
-        # Delete the uploaded archive
-        run("rm /tmp/{}".format(archive_filename))
-
-        # Move the files to the final location
-        run("mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/".format(archive_folder, archive_folder))
-
-        # Remove the empty web_static directory
-        run("rm -rf /data/web_static/releases/{}/web_static".format(archive_folder))
-
-        # Delete the old symbolic link (if exists)
-        run("rm -rf /data/web_static/current")
-
-        # Create a new symbolic link
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(archive_folder))
-
-        print("New version deployed!")
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(releases_path))
+        run('tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
+        run('rm -rf {}/web_static'.format(releases_path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(releases_path))
+        print('New version deployed!')
         return True
-
-    except Exception as e:
-        print("Deployment failed: {}".format(e))
+    except:
         return False
 
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='Distribute an archive to web servers.')
-    parser.add_argument('archive_path', type=str, help='Path to the archive')
-    parser.add_argument('ssh_key', type=str, help='SSH key file path')
-    parser.add_argument('username', type=str, help='SSH username')
-
-    return parser.parse_args()
-
-
-if __name__ == '__main__':
-    args = parse_arguments()
-    do_deploy(args.archive_path, args.ssh_key, args.username)
